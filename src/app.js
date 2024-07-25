c16032c36af149602ff333edaaaaa04030d542a1
Tgodwin94 <tyler.godwin94@gmail.com>
2024-07-25 00:11:24 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const excuse = document.querySelector("#excuse");
+
+const excuses = {
+  who: [
+    "My dog",
+    "The family Tyrannosaurus Rex",
+    "A flying whale",
+    "A special guest"
+  ],
+  action: ["peed on", "destroyed", "stared intensely at", "broke out of"],
+  what: ["its cage", "my car", "our new couch", "my house"],
+  when: ["today", "last night", "on Halloween", "last Tuesday"]
 };
+
+const randomElement = arr => {
+  let random = arr[Math.floor(Math.random() * arr.length)];
+  return random;
+};
+
+const randomExcuse = () => {
+  const parts = ["who", "action", "what", "when"];
+  const message = parts.map(part => randomElement(excuses[part])).join(" ");
+  excuse.innerHTML = message;
+};
+
+window.onload = randomExcuse;
